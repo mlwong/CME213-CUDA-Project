9479189aa758bf8b6368379baba5ab6b3f38d2e4
wongml <wongml@icme-gpu1.cm.cluster>
2015-05-02 16:26:36 -0700

    update code

--- hw3/code/starter-code/gpuStencil.cu
+++ hw3/code/starter-code/gpuStencil.cu
@@ -63,11 +63,11 @@ void gpuStencil(float *next, const float *curr, int gx, int nx, int ny,
 {
   // TODO
   
-  const int tid_x = threadIdx.x + blockDim.x * blockIdx.x;
-  const int tid_y = threadIdx.y + blockDim.y * blockIdx.y;
+  const int tid_x = threadIdx.x + blockDim.x*blockIdx.x;
+  const int tid_y = threadIdx.y + blockDim.y*blockIdx.y;
   
   // If the thread is not inside the domain, return
-  if ((tid_x >= nx) || (tid_y >= ny))
+  if (tid_x >= nx || tid_y >= ny)
   {
     return;
   }
@@ -99,13 +99,13 @@ double gpuComputation(Grid &curr_grid, const simParams &params) {
   Grid next_grid(curr_grid);
 
   // TODO: Declare variables/Compute parameters.
-  
+
   dim3 threads(0, 0);
   dim3 blocks(0, 0);
   
   // Set the size of each block
-  const unsigned int block_size = 192u;
   const unsigned int block_dim_x = 32u;
+  const unsigned int block_dim_y = 16u;
   
   int nx = params.nx();
   int ny = params.ny();
@@ -119,7 +119,7 @@ double gpuComputation(Grid &curr_grid, const simParams &params) {
   
   // Compute the block dimension
   threads.x = block_dim_x;
-  threads.y = block_size/block_dim_x;
+  threads.y = block_dim_y;
   
   // Assume each dimension of the block is less than 65536
   // and compute the grid size
@@ -188,7 +188,29 @@ __global__
 void gpuStencilLoop(float *next, const float *curr, int gx, int nx, int ny,
                     float xcfl, float ycfl)
 {
-  // TODO  
+  // TODO
+  
+  const int tid_x = threadIdx.x + blockDim.x*blockIdx.x;
+  const int tid_y = threadIdx.y + numYPerStep*blockDim.y*blockIdx.y;
+  
+  // If the thread is not inside the domain, return
+  if (tid_x >= nx)
+  {
+    return;
+  }
+  
+  // Compute the boarder size
+  const int b = order/2;
+  
+  for (int i = 0; i < numYPerStep; i++)
+  {
+    if (tid_y + i*blockDim.y >= ny)
+    {
+      return;
+    }
+    const int idx = (tid_y + i*blockDim.y + b)*gx + (tid_x + b);
+    next[idx] = Stencil<order> (&curr[idx], gx, xcfl, ycfl);
+  }
 }
 
 /**
@@ -209,8 +231,33 @@ double gpuComputationLoop(Grid &curr_grid, const simParams &params) {
   Grid next_grid(curr_grid);
   // TODO
   // TODO: Declare variables/Compute parameters.
+  
   dim3 threads(0, 0);
   dim3 blocks(0, 0);
+  
+  // Set the size of each block
+  const unsigned int block_dim_x = 64u;
+  const unsigned int block_dim_y = 8u;
+  const unsigned int numYPerStep = 8u;
+  
+  int nx = params.nx();
+  int ny = params.ny();
+  
+  int gx = params.gx();
+  
+  float xcfl = params.xcfl();
+  float ycfl = params.ycfl();
+  
+  int order = params.order();
+  
+  // Compute the block dimension
+  threads.x = block_dim_x;
+  threads.y = block_dim_y;
+  
+  // Assume each dimension of the block is less than 65536
+  // and compute the grid size
+  blocks.x = ((unsigned int) nx + threads.x - 1)/threads.x;
+  blocks.y = ((unsigned int) ny + threads.y*numYPerStep - 1)/(threads.y*numYPerStep);
 
   event_pair timer;
   start_timer(&timer);
@@ -221,6 +268,25 @@ double gpuComputationLoop(Grid &curr_grid, const simParams &params) {
     BC.updateBC(next_grid.dGrid_, curr_grid.dGrid_);
 
     // TODO: Apply stencil.
+    
+    switch (order)
+    {
+      case 2:
+        gpuStencilLoop<2, numYPerStep><<<blocks, threads>>>(next_grid.dGrid_, curr_grid.dGrid_,
+                                                            gx, nx, ny, xcfl, ycfl);
+        break;
+      case 4:
+        gpuStencilLoop<4, numYPerStep><<<blocks, threads>>>(next_grid.dGrid_, curr_grid.dGrid_,
+                                                            gx, nx, ny, xcfl, ycfl);
+        break;
+      case 8:
+        gpuStencilLoop<8, numYPerStep><<<blocks, threads>>>(next_grid.dGrid_, curr_grid.dGrid_,
+                                                            gx, nx, ny, xcfl, ycfl);
+        break;
+      default:
+        printf("ERROR: Order %d not supported", order);
+        exit(1);
+    }
 
     check_launch("gpuStencilLoop");
 
@@ -249,7 +315,50 @@ __global__
 void gpuShared(float *next, const float *curr, int gx, int gy,
                float xcfl, float ycfl)
 {
-  // TODO 
+  // TODO
+  
+  const int lane_x = threadIdx.x;
+  const int lane_y = threadIdx.y;
+  
+  // Compute the boarder size
+  const int b = order/2;
+  
+  __shared__ float smem_block[side*side];
+  
+  const int global_col = lane_x + (blockDim.x - order)*blockIdx.x;
+  
+  // Load the data into shared memory
+  if (global_col < gx)
+  {
+    const int y_lim = min(side, gy - (side - order)*blockIdx.y);
+    for (int i = lane_y; i < y_lim; i += blockDim.y)
+    {
+        const int global_row = i + (side - order)*blockIdx.y;
+        const int global_idx = global_row*gx + global_col;
+        const int smem_block_idx = i*blockDim.x + lane_x;
+        
+        smem_block[smem_block_idx] = curr[global_idx];      
+    }
+  }
+  
+  __syncthreads();
+  
+  // Do the computation here
+  if (global_col < gx - b)
+  {
+    if (lane_x >= b && lane_x < blockDim.x - b)
+    {
+      const int y_lim = min(side - b, gy - b - (side - order)*blockIdx.y);
+      for (int i = lane_y + b; i < y_lim; i += blockDim.y)
+      {
+        const int global_row = i + (side - order)*blockIdx.y;
+        const int global_idx = global_row*gx + global_col;
+        const int smem_block_idx = i*blockDim.x + lane_x;
+        
+        next[global_idx] = Stencil<order> (&smem_block[smem_block_idx], side, xcfl, ycfl);
+      }
+    }
+  }
 }
 
 /**
@@ -271,9 +380,33 @@ double gpuComputationShared(Grid &curr_grid, const simParams &params) {
   Grid next_grid(curr_grid);
  
   // TODO: Declare variables/Compute parameters.
+  
   dim3 threads(0, 0);
   dim3 blocks(0, 0);
-
+  
+  // Set the size of each block
+  const unsigned int block_dim_x = 64u;
+  const unsigned int block_dim_y = 8u;
+  const unsigned int smem_side = 64u;
+  
+  int nx = params.nx();
+  int ny = params.ny();
+  
+  int gx = params.gx();
+  int gy = params.gy();
+  
+  float xcfl = params.xcfl();
+  float ycfl = params.ycfl();
+  
+  // Compute the block dimension
+  threads.x = block_dim_x;
+  threads.y = block_dim_y;
+  
+  // Assume each dimension of the block is less than 65536
+  // and compute the grid size
+  blocks.x = ((unsigned int) nx + threads.x - (unsigned int) order - 1)/(threads.x - (unsigned int) order);
+  blocks.y = ((unsigned int) ny + smem_side - (unsigned int) order - 1)/(smem_side - (unsigned int) order);
+  
   event_pair timer;
   start_timer(&timer);
 
@@ -283,7 +416,26 @@ double gpuComputationShared(Grid &curr_grid, const simParams &params) {
     BC.updateBC(next_grid.dGrid_, curr_grid.dGrid_);
 
     // TODO: Apply stencil.
-
+    
+    switch (order)
+    {
+      case 2:
+        gpuShared<smem_side, 2><<<blocks, threads>>>(next_grid.dGrid_, curr_grid.dGrid_,
+                                                     gx, gy, xcfl, ycfl);
+        break;
+      case 4:
+        gpuShared<smem_side, 4><<<blocks, threads>>>(next_grid.dGrid_, curr_grid.dGrid_,
+                                                     gx, gy, xcfl, ycfl);
+        break;
+      case 8:
+        gpuShared<smem_side, 8><<<blocks, threads>>>(next_grid.dGrid_, curr_grid.dGrid_,
+                                                     gx, gy, xcfl, ycfl);
+        break;
+      default:
+        printf("ERROR: Order %d not supported", order);
+        exit(1);
+    }
+    
     check_launch("gpuShared");
 
     Grid::swap(curr_grid, next_grid);
